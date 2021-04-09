ebf82334eec4d3b2fce06a27995322d0ed27583c
jmhoerr <jmhoerr@uark.edu>
2021-04-09 06:48:13 -0500

    updated for assn7 question2

--- parse_GFF.py
+++ parse_GFF.py
@@ -2,7 +2,9 @@
 
 import csv
 import argparse
+import re 
 from Bio import SeqIO
+from collections import defaultdict
 
 # inputs: 1) GFF file, 2) corresponding genome sequence (FASTA format)
 
@@ -16,11 +18,15 @@ parser.add_argument("fasta", help='name of the FASTA file')
 # parse the arguments
 args = parser.parse_args()
 
+# dictionary: key = gene_name, value = sequence
+gene_dict = defaultdict(dict)
+
 
 # read in FASTA file
 genome = SeqIO.read(args.fasta, 'fasta')
 # print(genome.id)
 # print(genome.seq)
+
 
 
 # open and read in GFF file
@@ -35,17 +41,30 @@ with open(args.gff, 'r') as gff_in:
 		start  = line[3]
 		end    = line[4]
 		strand = line[6]
+		code   = line[2]
 		
-		print(header)
-		
-		# extract the sequence 
-		# print(genome.seq[int(start)-1:int(end)])
+		# print gene name 
+		header_split = header.split()
+		gene_name = 'Citrullus_lanatus_' + header_split[1]
+		exon_num = header_split[3]
+				
+					
+		# extract CDS from code list
+		if code == 'CDS':
+			
+			# calculate and return reverse complement of features on '-' strand
+			if strand == '+':
+				pos_strand = genome.seq[int(start)-1:int(end)]
+				print(gene_name)
+				print(pos_strand)
+				
 
-
-		# calculate and return reverse complement of features on '-' strand
-		if strand == '+':
-			print(genome.seq[int(start)-1:int(end)])
+			else:
+				neg_strand = genome.seq[int(start)-1:int(end)]
+				rev_comp = neg_strand.reverse_complement()
+				print(gene_name)
+				print(rev_comp)
+			
 		else:
-			neg_strand = genome.seq[int(start)-1:int(end)]
-			print(neg_strand.reverse_complement())
+			continue
 
